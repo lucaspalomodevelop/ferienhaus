704247a14b0976e281b72e1ddd0f8225f8d2c06b
lucaspalomodevelop <lucas.palomo@t-online.de>
2021-04-13 13:07:03 +0200

    Projektdateien hinzufügen.

---
+++ ferienhaus/Klassen/CSV.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ferienhaus.klassen
+{
+    class CSV
+    {
+
+        public static object ReadData()
+        {
+
+            return new object();
+        }
+
+        public static void WriteData(object obj)
+        {
+
+            var reader = new StreamReader(File.OpenRead(@"D:\New folder\Data.csv"));
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+                listA.Add(values[0]);
+                listB.Add(values[1]);
+                foreach (var coloumn1 in listA)
+                {
+                    Console.WriteLine(coloumn1);
+                }
+                foreach (var coloumn2 in listA)
+                {
+                    Console.WriteLine(coloumn2);
+                }
+            }
+
+
+        }
+
+    }
+}
---
+++ ferienhaus/Klassen/Data.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ferienhaus.Klassen
+{
+    public static class Data
+    {
+
+        public static object read()
+        {
+            var reader = new StreamReader(File.OpenRead(@".\Data.csv"));
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+                listA.Add(values[0]);
+                listB.Add(values[1]);
+                foreach (var coloumn1 in listA)
+                {
+                    Console.WriteLine(coloumn1);
+                }
+                foreach (var coloumn2 in listA)
+                {
+                    Console.WriteLine(coloumn2);
+                }
+            }
+            return new object();
+        }
+
+        public static void write(object OBJ)
+        {
+            var write = new StreamWriter(File.OpenWrite(@".\Data.csv"));
+
+        //    write.WriteLine(OBJ.toCSV());
+        }
+    }
+
+}
